aeb4930f39f668f026deded2d074b6ec888b41ed
Pavel Shuvalov <pshuvalov@doublescoring.com>
2022-10-15 12:48:16 +0300

    Tests for reminder owners

--- src/main/java/io/shuvalov/test/reminder/repository/ReminderRepository.java
+++ src/main/java/io/shuvalov/test/reminder/repository/ReminderRepository.java
@@ -4,11 +4,11 @@ import io.shuvalov.test.reminder.domain.Reminder;
 import io.shuvalov.test.reminder.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReminderRepository extends JpaRepository<Reminder, Long> {
-	List<Reminder> findAllByNotifiedIsFalseAndTimeLessThanEqual(OffsetDateTime time);
+	List<Reminder> findAllByNotifiedIsFalseAndTimeLessThanEqual(LocalDateTime time);
 
 	List<Reminder> findAllByOwner(User owner);
 }
